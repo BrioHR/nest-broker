ab31d2e3ba0dc6689dea146bb3b55868dc8ea3cf
Benjamin Bellantonio <contact@benjaminb.fr>
2020-05-10 15:41:27 +0800

    Added RabbitMQ reconnection system

--- src/adapters/rabbitmq.adapter.ts
+++ src/adapters/rabbitmq.adapter.ts
@@ -1,11 +1,11 @@
-import { Logger, LoggerService } from "@nestjs/common";
+import { Logger } from "@nestjs/common";
 
 import { BrokerAdapterInterface } from "./broker-adapter.interface";
 import { MODULE_NAME } from "./../constants";
 
 export class RabbitMQAdapter implements BrokerAdapterInterface {
   private logger: Logger;
-  constructor(private url: string, private service: string, private customLogger?: Logger) {
+  constructor(private url: string, private service: string, customLogger?: Logger) {
     this.logger = new Logger(MODULE_NAME);
     if (customLogger) {
       this.logger = customLogger;
@@ -14,19 +14,20 @@ export class RabbitMQAdapter implements BrokerAdapterInterface {
 
   public async publish(topic: string, content: {}): Promise<void> {
     try {
-      const connect = this.connect(this.url);
-      connect
-        .then(async (connection: any) => {
-          return connection.createChannel();
-        })
-        .then(async (channel: any) => {
-          this.logger.log(`Publish ${topic} ${JSON.stringify(content)}`);
+      this.connect(this.url, connect => {
+        connect
+          .then(async (connection: any) => {
+            return connection.createChannel();
+          })
+          .then(async (channel: any) => {
+            this.logger.log(`Publish ${topic} ${JSON.stringify(content)}`);
 
-          return Promise.all([
-            channel.assertExchange(topic, "fanout"),
-            channel.publish(topic, "", Buffer.from(JSON.stringify(content)))
-          ]);
-        });
+            return Promise.all([
+              channel.assertExchange(topic, "fanout"),
+              channel.publish(topic, "", Buffer.from(JSON.stringify(content)))
+            ]);
+          });
+      });
     } catch (e) {
       this.logger.error(e);
     }
@@ -34,39 +35,60 @@ export class RabbitMQAdapter implements BrokerAdapterInterface {
 
   public async subscribe(topic: string, callback: (message: string) => void): Promise<void> {
     try {
-      const connect = this.connect(this.url);
-      connect
-        .then(async (connection: any) => {
-          return connection.createChannel();
-        })
-        .then(async (channel: any) => {
-          const exchange = topic;
-          topic = `${this.service}_${topic}`;
+      this.connect(this.url, connect => {
+        connect
+          .then(async (connection: any) => {
+            return connection.createChannel();
+          })
+          .then(async (channel: any) => {
+            const exchange = topic;
+            topic = `${this.service}_${topic}`;
 
-          return Promise.all([
-            channel.assertQueue(topic),
-            channel.assertExchange(exchange, "fanout"),
-            channel.bindQueue(topic, exchange),
-            channel.consume(topic, async msg => {
-              this.logger.log(`Consume ${topic} ${msg.content.toString()}`);
-              if (msg !== null) {
-                try {
-                  callback(JSON.parse(msg.content.toString()));
-                  channel.ack(msg);
-                } catch (e) {
-                  // TODO handle nack
-                  channel.reject();
+            return Promise.all([
+              channel.assertQueue(topic),
+              channel.assertExchange(exchange, "fanout"),
+              channel.bindQueue(topic, exchange),
+              channel.consume(topic, async msg => {
+                this.logger.log(`Consume ${topic} ${msg.content.toString()}`);
+                if (msg !== null) {
+                  try {
+                    callback(JSON.parse(msg.content.toString()));
+                    channel.ack(msg);
+                  } catch (e) {
+                    // TODO handle nack
+                    channel.reject();
+                  }
                 }
-              }
-            })
-          ]);
-        });
+              })
+            ]);
+          });
+      });
     } catch (e) {
       this.logger.error(e);
     }
   }
 
-  private async connect(url: string): Promise<any> {
-    return require("amqplib").connect(url);
+  private connect(url: string, callback): any {
+    require("amqplib/callback_api").connect(url, (err, conn) => {
+      if (err) {
+        this.logger.error("[AMQP]", err.message);
+
+        return setTimeout(() => this.connect(url, callback), 3000);
+      }
+      conn.on("error", err => {
+        if (err.message !== "Connection closing") {
+          this.logger.error("[AMQP] conn error", err.message);
+        }
+      });
+      conn.on("close", () => {
+        this.logger.error("[AMQP] reconnecting");
+
+        return setTimeout(() => this.connect(url, callback), 3000);
+      });
+
+      this.logger.log("[AMQP] connected");
+
+      callback(Promise.resolve(conn));
+    });
   }
 }
